7777a8be837b3ac3ad6b245869e2c767090a5966
Alexander Elyseev <alexander.elyseev@gmail.com>
2014-06-27 13:10:42 +0600

    Example of possible problem in catching exception in iterators.

---
+++ IteratorBlockExample/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace IteratorBlockExample
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Examples of implementing and using iterators.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Aplication entry point.
+        /// </summary>
+        static void Main()
+        {
+            IteratorExceptionExample();
+        }
+
+        /// <summary>
+        /// Example of handling exceptions in iterator.
+        /// </summary>
+        static void IteratorExceptionExample()
+        {
+            try
+            {
+                // Iterator block will be executed only on calling MoveNext.
+                // See standard 10.14.4.
+                IEnumerable<int> ints = GetIteratorWithException();
+
+                Console.WriteLine("This will be written.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("This will never be written.");
+            }
+        }
+
+        /// <summary>
+        /// Example of iterator that always throws an exception.
+        /// Subtle thing is that exception will be generated only when iteration starts.
+        /// </summary>
+        /// <returns>Some collection.</returns>
+        static IEnumerable<int> GetIteratorWithException()
+        {
+            throw new InvalidOperationException();
+            yield return 42;
+        }
+    }
+}
---
+++ IteratorBlockExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IteratorBlockExample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IteratorBlockExample")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d73fd31a-8af8-498e-81ba-cf2a2831f1c5")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
