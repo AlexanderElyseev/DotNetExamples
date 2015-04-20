ab981eea05b62e5e09cfdf9ad9855f5043deb8f2
Alexander Elyseev <alexander.elyseev@gmail.com>
2015-04-21 00:58:25 +0500

    Add example of usage __arglist in C#

---
+++ ArglistExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ArglistExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Print(__arglist(3, 3, 4));
+        }
+
+        static void Print(__arglist)
+        {
+            var it = new ArgIterator(__arglist);
+            var n = it.GetRemainingCount();
+            for (int i = 0; i < n; i++)
+            {
+                TypedReference reference = it.GetNextArg();
+
+                Type type = __reftype(reference);
+                int val = __refvalue(reference, int);  // Only ints here (or InvalidCastException).
+
+                Console.WriteLine("Type: {0}, Val: {1}.", type, val);
+            }
+        }
+    }
+}
---
+++ ArglistExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ArglistExample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ArglistExample")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("640586b8-006c-4748-9ff9-fa9ba347d9e4")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
